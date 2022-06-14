71147452a134d50690cac17552659883247448f3
Sergey Belov <kort.dll@gmail.com>
2022-06-15 00:10:05 +0300

    fet: add publish-fake-messages module

---
+++ core/publish-fake-messages.js
@@ -0,0 +1,27 @@
+import { sentence } from 'txtgen'
+import { KurdishNicknames } from 'kurdish-nicknames'
+
+function generateMessage() {
+  const { first_name, last_name } = KurdishNicknames.generate()
+  return {
+    username: `${first_name} ${last_name}`,
+    time: Date.now(),
+    value: sentence()
+  }
+}
+
+export function generateFakeMessages(wss, interval = 5000) {
+  setInterval(async () => {
+    const data = generateMessage()
+
+    await wss.messages.push(data)
+
+    wss.instance.clients.forEach(client => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(
+          JSON.stringify({ type: 'push', data })
+        )
+      }
+    })
+  }, interval)
+}
